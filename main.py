c525a92961c9b6a3c0f9e9c6c23dc2029dd2240a
Fayyaz Hussain Shah <fayyazhs107@gmail.com>
2026-07-25 11:49:50 +0500

    1

--- main.py
+++ main.py
@@ -26,37 +26,37 @@ def main():
     from configs import config_setup
     config_setup.test_setup()
 
-    # # ---------------------------- 2. Get Data ------------------------------- #
-    # from src.data import fetch_data
-    # df_raw = fetch_data.get_raw_data()
-    #
-    # # ---------------------------- 3. Pre-process Data ------------------------------- #
-    # from src.data import pre_process_data
-    # pre_process_data.data_pre_processing(df_raw)
-    #
-    # # ---------------------------- 4.EDA ------------------------------- #
-    # from src.data import exp_data_analysis
-    # exp_data_analysis.eda()
-    #
-    # # ---------------------------- 5. Processing ------------------------------- #
-    # from src.processing import process
-    # process.process_main()
-    #
-    # # ---------------------------- 6. Models Training ------------------------------- #
-    # from src.models import models_training
-    # models_training.models_training_main()
-    #
-    # # ---------------------------- 7. Models Testing ------------------------------- #
-    # from src.models import models_testing
-    # models_testing.models_testing_main()
-    #
-    # # ---------------------------- 8. Models Analysis ------------------------------- #
-    # from src.models import models_analysis
-    # models_analysis.models_analysis_main()
-    #
-    # # ---------------------------- 9. Final Analysis ------------------------------- #
-    # from src import final_results
-    # final_results.final_results_main()
+    # ---------------------------- 2. Get Data ------------------------------- #
+    from src.data import fetch_data
+    df_raw = fetch_data.get_raw_data()
+
+    # ---------------------------- 3. Pre-process Data ------------------------------- #
+    from src.data import pre_process_data
+    pre_process_data.data_pre_processing(df_raw)
+
+    # ---------------------------- 4.EDA ------------------------------- #
+    from src.data import exp_data_analysis
+    exp_data_analysis.eda()
+
+    # ---------------------------- 5. Processing ------------------------------- #
+    from src.processing import process
+    process.process_main()
+
+    # ---------------------------- 6. Models Training ------------------------------- #
+    from src.models import models_training
+    models_training.models_training_main()
+
+    # ---------------------------- 7. Models Testing ------------------------------- #
+    from src.models import models_testing
+    models_testing.models_testing_main()
+
+    # ---------------------------- 8. Models Analysis ------------------------------- #
+    from src.models import models_analysis
+    models_analysis.models_analysis_main()
+
+    # ---------------------------- 9. Final Analysis ------------------------------- #
+    from src import final_results
+    final_results.final_results_main()
 
     logging.info(f'END')
 
